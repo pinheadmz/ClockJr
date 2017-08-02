40b19d7785d6cad625f2e93df6fdac5cf6fe62d5
Matthew Zipkin <pinheadmz@gmail.com>
2017-08-02 02:34:25 +0000

    restart dhcp

--- wififiles/adhocOFF.sh
+++ wififiles/adhocOFF.sh
@@ -2,8 +2,10 @@
 
 sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
+sudo systemctl stop dhcpcd
+
 sudo cp ./dhcpcd.conf.old /etc/dhcpcd.conf
 sudo cp ./interfaces.old /etc/network/interfaces
-sudo service dhcpcd restart
+sudo systemctl start dhcpcd
 sudo ifdown wlan0
 sudo ifup wlan0
